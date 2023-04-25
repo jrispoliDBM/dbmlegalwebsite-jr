ed329887507414700dc7a78d7d555257da68eb1b
Lloyd Mandell <llmandell@gmail.com>
2023-04-25 07:44:29 -0400

    Fixing picture cache

--- src/content/team.js
+++ src/content/team.js
@@ -2,70 +2,70 @@ export const team = [
     {
         name: 'Michelle Bergman',
         title: 'Co-Founder and CEO',
-        image: '/images/team/michelle-bergman-bw.png',
+        image: '/images/team/michelle-bergman.png',
         founder: true,
         bio: "Michelle D. Bergman has over 30 years of legal and business experience. She has served in multiple C-Suite roles, including General Counsel, at companies such as Duane Reade Inc. (the New York-based drug store chain), Barneys (the upscale department store chain), and MediMedia Health (a multidisciplinary health services company). She began her career at Latham & Watkins and, from there, moved in-house to several investment banks and private equity firms. In 2011, Ms. Bergman founded Bergman Law, a corporate law boutique law firm. Ms. Bergman and Ms. Mandell joined forces in 2019 to create an expanded and rebranded distributed law firm model, focusing solely on corporate law, and in 2021 launched DBM."
     },
     {
         name: 'Maura Mandell',
         title: 'Co-Founder and COO',
-        image: '/images/team/maura-mandell-bw.png',
+        image: '/images/team/maura-mandell.png',
         founder: true,
         bio:"Ms. Mandell is a corporate attorney who also has significant operational experience running and consulting to growth companies. Ms. Mandell practiced law in the corporate departments of Weil, Gotshal, and Manges LLP and Fischbein, Badillo, Wagner, Harding, LLP. She was the founder and CEO of two startups — an organic skincare brand and a consumer wellness service. Additionally, Ms. Mandell has provided legal and operational consulting services to several businesses, including a large healthcare-focused hedge fund. Ms. Mandell and Ms. Bergman met in 2019 and quickly realized that they shared the vision that there is a better way to practice law. They began working together in early 2020 to build the law firm of the future and launched DBM in late 2020. Ms. Mandell focuses on all of the firm's operational aspects while simultaneously finding ways to drive its strategic growth.",
     },
     {
         name: 'Counsel McCullen',
         title: 'Senior Partner',
-        image: '/images/team/counsel-mccullen-bw.png',
+        image: '/images/team/counsel-mccullen.png',
         founder: false,
         bio : "Counsel McCullen is a corporate and securities attorney with substantial experience representing emerging companies and investment firms throughout their entire life cycle. Specifically, he specializes in business and fund formations, founder agreements, equity, debt financing, mergers and acquisitions, incentive equity programs, and private credit transactions. His clients include entrepreneurs, emerging and established companies, executives, angel and venture investors, and investment firms. With a focus on negotiated transactions, Counsel strives to ensure that business goals are achieved, and deals are closed quickly and efficiently.\n\nCounsel is admitted to practice law in New York and Georgia. He currently serves on the Advisory Board of Georgia Lawyers for the Arts, a non-profit organization that provides legal assistance to artists and arts organizations. Counsel earned his B.A. in Political Science and Philosophy from Georgia State University and his J.D. from Georgia State University College of Law, where he clerked for Georgia Supreme Court Presiding Justice P. Harris Hines."
     },
     {
         name: 'Anthony Grafitti',
         title: 'Senior Partner',
-        image: '/images/team/anthony-grafitti-bw.png',
+        image: '/images/team/anthony-grafitti.png',
         founder: false,
         bio : "Mr. Grafitti is a commercial real estate and finance lawyer with approximately 25 years experience in all aspects of transactional and distressed real estate and finance. Mr. Grafitti has participated as a lawyer, advisor, broker, lender, and investor in many notable real estate transactions in both up and down markets. His broad experience and track record brings dynamic hands-on experience in all facets of real estate investment and ownership, including, acquisitions, dispositions, development, operations, construction and property management, leasing, finance and legal. Representative clients include publicly traded single-tenant, multi-tenant and apartment REITs, investment funds, private equity firms, large institutions, family offices, endowments, on balance-sheet lenders, and CMBS Special and Master Servicers. In addition to his broad experience representing some of the nation’s most sophisticated investors, Mr. Grafitti has deep experience in resolving, repositioning and recapitalizing distressed real estate assets, including, loan modifications, discounted payoffs, AB note restructures, receiverships, non-judicial foreclosures and whole loan sales.\n\nMr. Grafitti began his legal career at Fennemore Craig, P.C., the oldest law firm in Arizona, and subsequently was a key member of Ballard Spahr LLP’s Distressed Real Estate Initiative and its Real Estate and Real Estate Finance Transactional Group. Mr. Grafitti practices from Duncan Bergman Mandell’s Scottsdale, Arizona office"
     },
     {
         name: 'Kevin McGraw',
         title: 'Senior Partner',
-        image: '/images/team/kevin-mcgraw-bw.png',
+        image: '/images/team/kevin-mcgraw.png',
         founder: false,
         bio : "Kevin McGraw is a highly accomplished corporate and business attorney, with demonstrated success in complex transaction management, litigation management, dispute resolution, government contracting, regulatory compliance, and policy development, and has a proven ability to negotiate complex business transactions.\n\nDuring a 21-year career serving as in-house counsel (Assistant Geneneral Counsel and ad hoc General Counsel) for several mid-to-large size domestic and multi-national corporations, Kevin acted primarily as a corporate generalist, but also supported specific departments at various times, including but not limited to National Accounts (sales), Procurement, Finance and Accounting, Facilities, Human Resources, Marketing, and InfoSec departments.  One of the areas he has enjoyed most has been working on divestitures, and mergers and acquisitions, helping to facilitate transactions including due diligence, ancillary agreements, drafting and negotiation of representations and warranties, and managing outside M&A counsel.\n\nIn his free time, Kevin volunteers through the State Bar of Texas with the Texas Lawyers for Texas Veterans Pro Bono Initiative. Kevin has been happily married to his wife Nancy for almost 35 years. He also loves golfing, hunting, and watching sports, especially with his three sons."
     },
     {
         name: 'Heather J. Haase',
         title: 'Senior Counsel',
-        image: '/images/team/heather-haase-bw.png',
+        image: '/images/team/heather-haase.png',
         founder: false,
         bio : "Heather J. Haase is a corporate and business attorney based in New York, with several years of experience, advising public and private companies on business matters, securities, and general corporate law. Heather began her career in the corporate departments of Jones Day and Vinson & Elkins LLP, where her practice focused on corporate finance, mergers and acquisitions, securities offerings, and ongoing public company securities disclosure. She later spent several years at a \“boutique\” firm in lower Manhattan, where she helped start and build a thriving business and securities law practice to add to its existing litigation practice. While there, she also gained experience litigating fraud cases brought by the Securities Exchange Commission and “forex” fraud cases brought by the Commodity Futures Trading Commission, which has brought a unique perspective to her work as a corporate and securities attorney. Most recently, she has worked in the private equity arena, supporting acquisitions through helping to develop and promote best practices around non-disclosure agreements and related documents."
     },
     {
         name: 'Stephanie Mongelluzzo',
         title: 'Senior Counsel',
-        image: '/images/team/stephanie-ann-michael-bw.png',
+        image: '/images/team/stephanie-ann-michael.png',
         founder: false,
         bio : null
     },
     {
         name: 'Lori Ann Fox',
         title: 'Senior Counsel',
-        image: '/images/team/lori-ann-fox-bw.png',
+        image: '/images/team/lori-ann-fox.png',
         founder: false,
         bio : null
     },
     {
         name: 'Joseph Rispoli',
         title: 'Senior Director People & Business Strategy',
-        image: '/images/team/joseph-rispoli-bw.jpg',
+        image: '/images/team/joseph-rispoli.jpg',
         founder: false,
         bio : null
     },
     {
         name: 'Jenna Carr',
         title: 'Executive Assistant',
-        image: '/images/team/jenna-carr-bw.png',
+        image: '/images/team/jenna-carr.png',
         founder: false,
         bio : null
     }
