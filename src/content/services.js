03da0a13542e5ee2315fcc0dfa28eff7d987b918
Lloyd Mandell <llmandell@gmail.com>
2023-04-28 16:29:47 -0400

    1

--- src/content/services.js
+++ src/content/services.js
@@ -1,5 +1,5 @@
 import { corporateDetails, financeDetails, regulatoryAndComplianceDetails, intellectualPropertyDetails, technologyDetails, realEstateDetails, employmentAndLaborLawDetails, trustsAndEstateDetails, taxDetails, familyOfficeDetails } from './services-details';
-//ch
+//ch1
 
 
 export const services = [
